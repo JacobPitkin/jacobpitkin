61aa7cd05874dfb8c33ab9e336ded7f9a86b232c
Jacob Pitkin <jacob.pitkin.78@gmail.com>
2023-03-03 13:06:31 -0800

    Images for projects, started the JSON file for projects

---
+++ src/data/projects.js
@@ -0,0 +1,84 @@
+export default [
+  {
+    name: 'Journal Editor',
+    tech: [
+      'Java',
+      'JavaScript',
+      'Tomcat',
+      'SQL',
+      'FOP',
+      'XML'
+    ],
+    images: [
+      '/journal-editor.png',
+      '/journal-editor-menu.png',
+      '/journal-editor-macro-sidebar.png',
+      '/journal-editor-committee-selector.png',
+      '/journal-editor-bill-import-list.png',
+      '/journal-editor-with-text.png',
+      '/journal-editor-macro-list.png',
+      '/macro-editor.png',
+      '/macro-editor-options.png'
+    ]
+  },
+  {
+    name: 'Transmittal Letter Editor',
+    tech: [
+      'Java',
+      'JavaScript',
+      'Tomcat',
+      'SQL',
+      'FOP',
+      'XML',
+      'XSLT'
+    ],
+    images: [
+      '/transmittal-letter.png'
+    ]
+  },
+  {
+    name: 'Journal Error Report',
+    tech: [
+      'Java',
+      'Tomcat',
+      'FOP',
+      'XML',
+      'XSLT'
+    ]
+  },
+  {
+    name: 'Bill Status Verification System',
+    tech: [
+      'Java',
+      'JavaScript',
+      'Tomcat',
+      'SQL'
+    ],
+    images: [
+      '/bill-status.png'
+    ]
+  },
+  {
+    name: 'Closed Captioning System',
+    tech: [
+      'Java',
+      'Java Swing',
+      'Wowza Streaming Server',
+      'SQL'
+    ]
+  },
+  {
+    name: 'Legislative XSLT\'s',
+    tech: [
+      'XML',
+      'XSLT',
+      'FOP'
+    ]
+  },
+  {
+    name: 'Discord Management Bot',
+    tech: [
+      'Node.js'
+    ]
+  }
+];
