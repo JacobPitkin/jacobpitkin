23f630d19d0c03a33baa68bead9522c1afb76fe4
Jacob Pitkin <jacob.pitkin.78@gmail.com>
2023-03-03 17:19:28 -0800

    Started a carousel, but moving towards tailwind implementation

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,6 +1,16 @@
+import { Carousel } from "react-responsive-carousel";
+
 function ProjectItem({ name, shortDescription, tech, images }) {
   return (
     <div className="pt-8">
+      <Carousel className="flex-row">
+        {images.map((image) => (
+          <div>
+            <img src={image} />
+            <p className="legend">{image}</p>
+          </div>
+        ))}
+      </Carousel>
       <div>{name}</div>
       <div className="text-start">{shortDescription}</div>
     </div>
